3856084df21f4853ef78c9f447351862260162e7
Mayank Rawat <mrwt005@gmail.com>
2020-12-28 11:31:58 +0530

    Fixed Bugs

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -41,7 +41,7 @@ const Header = () => {
                 <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
                 <HeaderOption Icon={ChatIcon} title='Messaging'/>
                 <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
-                <HeaderOption avatar={user?.photoURL} alt={user?.email[0]} title='me' onClick={logoutOfApp}/>
+                <HeaderOption avatar={user?.photoURL} alt={user?.email[0]} title='Me' onClick={logoutOfApp}/>
             </div>
             
         </div>
